bdee601bb01d926fd91bbb8af258299cd259b1db
Adurizz <nickhs988@gmail.com>
2022-12-05 04:40:13 +0900

    지도 4차 commit(지도 위치 빼고 게시글 게시 성공)

--- app/src/main/java/com/example/practice/LocationActivity.kt
+++ app/src/main/java/com/example/practice/LocationActivity.kt
@@ -3,28 +3,44 @@ package com.example.practice
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.practice.databinding.ActivityLocationBinding
 
 class LocationActivity : AppCompatActivity() {
     val binding by lazy { ActivityLocationBinding.inflate(layoutInflater)}
+    lateinit var getResult : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.mapBtn.setOnClickListener {
-            val intent = Intent(this, MapActivity::class.java)
-            startActivity(intent)
-        }
-
         var locations = mutableListOf<LocInfo>()
-
-        val item1 =LocInfo("Restaurant", "08-22", "It was good!")
-        locations.add(item1)
 
         val locAdapter = LocAdapter(locations)
         binding.locRecView.adapter = locAdapter
         binding.locRecView.layoutManager =LinearLayoutManager(this)
+
+        getResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            if (it.resultCode == RESULT_OK) {
+                val title = it.data?.getStringExtra("title")
+                val date = it.data?.getStringExtra("date")
+                val cont = it.data?.getStringExtra("content")
+                val lat = it.data?.getStringExtra("latitude")
+                val long = it.data?.getStringExtra("longitude")
+                if (title != null && date!= null && cont!=null && lat!=null && long!=null) {
+                    Log.d("ITM", "$title, $date, $cont")
+                    locations.add(LocInfo(title,date, cont))
+                    locAdapter.notifyDataSetChanged()
+                }
+            }
+        }
+
+        binding.mapBtn.setOnClickListener {
+            val intent = Intent(this, MapActivity::class.java)
+            getResult.launch(intent)
+        }
     }
 }
--- app/src/main/java/com/example/practice/MapActivity.kt
+++ app/src/main/java/com/example/practice/MapActivity.kt
@@ -1,8 +1,11 @@
 package com.example.practice
 
+import android.content.Intent
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.practice.databinding.ActivityLocationBinding
 import com.example.practice.databinding.ActivityMapBinding
 
@@ -20,6 +23,23 @@ class MapActivity : AppCompatActivity(), MapsFragment.OnLocationPassListener {
 
         transaction.add(binding.mapFrame.id, mapFrangment)
         transaction.commit()
+
+        binding.savBtn.setOnClickListener {
+            if (binding.latitudeText.text == "") {
+                Toast.makeText(this,"please select location!", Toast.LENGTH_SHORT).show();
+            }
+            else {
+                val intent = Intent(this, LocationActivity::class.java).apply {
+                    putExtra("title", binding.mapTitle.text.toString())
+                    putExtra("date", binding.mapDate.text.toString())
+                    putExtra("content",binding.mapContent.text.toString())
+                    putExtra("latitude",binding.latitudeText.text.toString())
+                    putExtra("longitude", binding.longtitudeText.text.toString())
+                }
+                setResult(RESULT_OK,intent)
+                finish()
+            }
+        }
     }
 
     override fun onLocationPass(lat: String, lng: String) {
