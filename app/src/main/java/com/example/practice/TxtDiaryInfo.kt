3b5c28de81a308fc91cf63443e8d353afe890150
Adurizz <nickhs988@gmail.com>
2022-12-09 23:48:39 +0900

    텍스트 일기 db 저장 완료

--- app/src/main/java/com/example/practice/TxtDiaryInfo.kt
+++ app/src/main/java/com/example/practice/TxtDiaryInfo.kt
@@ -2,5 +2,6 @@ package com.example.practice
 
 data class TxtDiaryInfo(
     var diaryContents : String? = null,
-    var diaryImgSrc : Int? = null
+    var diaryImgSrc : Int? = null,
+    var diaryDate : String? = null
 )
